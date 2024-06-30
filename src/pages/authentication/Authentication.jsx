5c253a812bbca6b712b2cde7847b2cd2734275a7
PrathamK78 <pratham1054.be21@chitkara.edu.in>
2024-06-30 18:01:59 +0530

    Update bg

--- src/pages/authentication/Authentication.jsx
+++ src/pages/authentication/Authentication.jsx
@@ -1,9 +1,20 @@
 import { Outlet } from "react-router-dom";
 import { loginPageHeroImg } from "../../utils/constants";
+//import twitterBg from '../assets/twitter-bg.jpg'; 
 
 const Authentication = () => {
+
+  const pageStyle = {
+    backgroundImage: "url('../assets/twitter-bg.jpg')",
+    backgroundSize: "cover",
+    backgroundPosition: 'center', // Center the background image
+    backgroundRepeat: 'no-repeat', // Do not repeat the image
+    height: '100vh', // Make sure to cover the full viewport height
+    width: '100vw',
+  };
+
   return (
-    <div className="bg-lighterPrimary min-h-screen flex items-center justify-center">
+    <div style={pageStyle} className="min-h-screen flex items-center justify-center">
       <div className="bg-[white] flex rounded-2xl shadow-2xl max-w-3xl m-4 min-h-[600px] md:p-5 ">
         <Outlet />
         <div className=" md:flex hidden w-1/2 ">
@@ -15,6 +26,7 @@ const Authentication = () => {
         </div>
       </div>
     </div>
+    
   );
 };
 
